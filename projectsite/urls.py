f16a85e4b04a0397b689e881eb9e9f1b4c0384a1
Paul <mendozapaul477@gmail.com>
2026-04-07 16:57:47 +0800

    fix pwa

--- projectsite/urls.py
+++ projectsite/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from tasks import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', include('pwa.urls')),
     path('accounts/', include('allauth.urls')),
     
     # Home
